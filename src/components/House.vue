cbd5556d55754b8f8002a07d05e5d1a161d10c21
big-daddy-dom <dominicrupert@gmail.com>
2022-04-27 14:38:45 -0600

    2:38

---
+++ src/components/House.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="col-md-4 mb-2">
+        <div class="bg-light shadow rounded">
+            <router-link :to="{ name: 'HouseDetailsPage', params: { id: house.id} }">
+                <img
+                :src="house.imgUrl"
+                :alt="`${house.bedrooms} ${house.bathrooms} ${house.levels}`"
+                class="img-fluid rounded"
+                />
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+import {House} from '../models/House'
+export default {
+    props: {
+        house:{
+            type: House,
+            required: true,
+        },
+    },
+    setup(){
+        return{};
+    },
+};
+    
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
---
+++ src/components/HouseForm.vue
@@ -0,0 +1,23 @@
+<template>
+    <form @submit.prevent="handleSubmit()">
+        <div class="form-group">
+            <label for="bedrooms" class="form-label">Bedrooms:</label>
+            <input type="number"
+            class="form-control"
+            id="bedrooms"
+            v-model="editable.bedrooms"
+            />
+            
+        </div>
+        <div class="form-group">
+            <label for="bathrooms" class="form-label">Bathrooms:</label>
+            <input 
+            type="number"
+            class="form-control"
+            id="bathrooms"
+            v-model="editable.bathrooms"
+            />
+        </div>
+        <div class="form-group"></div>
+    </form>
+</template>
